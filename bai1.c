3146004f654f02762c75370206fbd3f7e06928d0
Zenkixz <senjixz@gmail.com>
2026-02-26 08:52:55 +0700

    com108-cs21301-lab7-AnhQuan

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [NGUYỄN HUỲNH ANH QUÂN]
+ * MSSV:      [PS48285]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH ĐẾM NGUYÊN ÂM VÀ PHỤ ÂM CỦA 1 CHUỖI 
@@ -9,6 +9,28 @@
 //  Output: Số lượng nguyên âm và phụ âm trong chuỗi 
 //  Biết rằng: Nguyên âm gồm: a,e,i,o,y,u
 
-
-// VIẾT CODE Ở ĐÂY
+//VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+main ()
+{
+    char str[100];
+    int i, nguyen_am = 0, phu_am = 0;
+    printf("Nhap vao 1 chuoi: ");
+    gets(str);
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'y' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'Y')
+        {
+            nguyen_am++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            phu_am++;
+        }
+    }
+    printf("So luong nguyen am: %d\n", nguyen_am);
+    printf("So luong phu am: %d\n", phu_am);
+}
 
